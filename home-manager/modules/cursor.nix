78dca0df4cbd87c2b26c33d915ef7322e81ad96c
Andrey0189 <varnavsky06@gmail.com>
2024-04-17 07:58:38 +0500

    Cursor size 36 -> 24

--- home-manager/modules/cursor.nix
+++ home-manager/modules/cursor.nix
@@ -3,7 +3,7 @@
     pointerCursor = {
       package = pkgs.vanilla-dmz;
       name = "Vanilla-DMZ";
-      size = 36;
+      size = 24;
       gtk.enable = true;
       x11 = {
         enable = true;
